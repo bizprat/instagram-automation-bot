7d638f3d335d04bf9e6bb85802d6969a688a16b6
bizprat <bizprat@gmail.com>
2021-05-15 13:39:44 +0530

    Added variables for settings

--- instagram.js
+++ instagram.js
@@ -1,16 +1,35 @@
 'use strict'
 
 const { shuffle } = require('lodash')
-const UserAgent = require('user-agent')
+const UserAgent = require('user-agents')
 
 const [ minMs, hourMs, dayMs ] = [60000, 3600000, 86400000]
 
 const ig = async (settings) => {
     
     const {
-        instagramBaseUrl = 'https://wwww.instagram.com/',
-        
-    }
+        instagramBaseUrl = 'https://www.instagram.com/',
+
+        username = process.env.INSTAGRAM_USERNAME,
+        password = process.env.INSTAGRAM_PASSWORD,
+
+        randomUserAgent = true,
+        userAgent,
+
+        maxFollowsPerHour = 20,
+        maxFollowsPerDay = 100,
+
+        maxLikesPerhour = 30,
+        maxLikesPerDay = 150,
+
+        cookiePath = './db/cookies.json',
+
+        excludeUsers = [],
+        excludeHashgtag = [],
+
+        followWithMinFollowing = 100,
+
+    } = settings
 
 }
 
